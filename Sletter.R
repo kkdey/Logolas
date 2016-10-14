4c5b3938a9ba08db044489ba4c03d09a1782cbf9
Kushal K Dey <kshldey@gmail.com>
2016-10-14 14:00:32 -0500

    added letter S

---
+++ Sletter.R
@@ -0,0 +1,55 @@
+
+
+#############  letter  S   ####################################
+
+
+grid.newpage()
+pushViewport(viewport(x=0.5,y=0.5,width=1, height=1,
+                      clip=TRUE))
+grid.rect(gp=gpar(col="grey"))
+
+angle1 <- c(seq((pi/2), 3*(pi/2), length.out=100))
+
+
+y.l1 <- 0.70 + 0.20*sin(angle1)
+x.l1 <- 0.5 + 0.35*cos(angle1)
+
+x_out <- c(1, x.l1)
+y_out <- c(1, y.l1)
+
+angle2 <- c(seq(-(pi/2), (pi/2), length.out=100))
+
+y.l2 <- 0.25 + 0.25*sin(angle2)
+x.l2 <- 0.5 + 0.5*cos(angle2)
+
+y.l3 <- 0.25 + 0.15*sin(angle2)
+x.l3 <- 0.5 + 0.3*cos(angle2)
+
+y.l4 <- 0.70 + 0.30*sin(angle1)
+x.l4 <- 0.5 + 0.5*cos(angle1)
+
+x <- c(0.85, x.l1, rev(x.l2), 0.1, 0.1, 0.5, x.l3, rev(x.l4), 0.8)
+y <- c(0.9, y.l1, rev(y.l2), 0, 0.1, 0.1, y.l3, rev(y.l4), 1)
+
+id <- c(rep(1, length(x)))
+
+fill_symbol <- TRUE
+
+if(fill_symbol){
+  grid.polygon(x, y,
+               default.unit="native",
+               id=id,
+               gp=gpar(fill=c("green","white"), 
+                       lwd=10))
+}else{
+  grid.polygon(x, y,
+               default.unit="native",
+               id=id,
+               gp=gpar(col="green", 
+                       lwd=10))
+}
+
+
+
+
+
