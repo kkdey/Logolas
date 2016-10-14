97ba7f524bef4d2909d3f02b24ce83adcd57a20c
Kushal K Dey <kshldey@gmail.com>
2016-10-14 13:12:14 -0500

    added letters O and Q

--- Qletter.R
+++ Qletter.R
@@ -1,13 +1,11 @@
 
 
-#############   Letter  O  ###########################
-
+##############   letter   Q   ################################
 
 grid.newpage()
 pushViewport(viewport(x=0.5,y=0.5,width=1, height=1,
                       clip=TRUE))
 grid.rect(gp=gpar(col="grey"))
-
 
 angle <- c(seq(0, 2*pi, length.out=100))
 
@@ -18,10 +16,13 @@ x.l1 <- 0.5 + 0.45*cos(angle)
 y.l2 <- 0.5 + 0.3*sin(angle)
 x.l2 <- 0.5 + 0.3*cos(angle)
 
-id <- c(rep(1, length(x.l1)), rep(2, length(x.l2)))
+x.l3 <- c(0.6, 0.8, 1, 0.8)
+y.l3 <- c(0.3, 0.3, 0, 0)
 
-x <- c(x.l1, x.l2)
-y <- c(y.l1, y.l2)
+id <- c(rep(1, length(x.l1)), rep(2, length(x.l2)), rep(3, length(x.l3)))
+
+x <- c(x.l1, x.l2, x.l3)
+y <- c(y.l1, y.l2, y.l3)
 
 fill_symbol <- TRUE
 
@@ -29,7 +30,7 @@ if(fill_symbol){
   grid.polygon(x, y,
                default.unit="native",
                id=id,
-               gp=gpar(fill=c("green","white"), 
+               gp=gpar(fill=c("green","white", "green"), 
                        lwd=10))
 }else{
   grid.polygon(x, y,
