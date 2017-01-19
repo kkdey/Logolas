ee779ee38e3e7c40afafac6a9430b638207ef6e7
Kushal K Dey <kshldey@gmail.com>
2017-01-19 10:23:46 -0600

    added a documentation bar

---
+++ docs/documentation/test/C_lowercase_letter.R
@@ -0,0 +1,74 @@
+
+##############   lower case c letter   #################################
+
+cletter <- function(plot=FALSE,
+                    fill_symbol = TRUE,
+                    colfill="green",
+                    lwd=10){
+
+  angle1 <- seq(0.3+pi/2,pi,length=100)
+  angle2 <- seq(pi,1.5*pi,length=100)
+  x.l1 <- 0.5 + 0.5*sin(angle1)
+  y.l1 <- 0.5 + 0.5*cos(angle1)
+  x.l2 <- 0.5 + 0.5*sin(angle2)
+  y.l2 <- 0.5 + 0.5*cos(angle2)
+
+  x.l <- c(x.l1,x.l2)
+  y.l <- c(y.l1,y.l2)
+
+  x <- c(x.l,rev(x.l))
+  y <- c(y.l,1-rev(y.l))
+
+  x.i1 <- 0.5 +0.35*sin(angle1)
+  y.i1 <- 0.5 +0.35*cos(angle1)
+  x.i1 <- x.i1[y.i1<=max(y.l1)]
+  y.i1 <- y.i1[y.i1<=max(y.l1)]
+  y.i1[1] <- max(y.l1)
+
+  x.i2 <- 0.5 +0.35*sin(angle2)
+  y.i2 <- 0.5 +0.35*cos(angle2)
+
+  x.i <- c(x.i1,x.i2)
+  y.i <- c(y.i1,y.i2)
+
+  x1 <- c(x.i,rev(x.i))
+  y1 <- c(y.i,1-rev(y.i))
+
+  x <- c(x,rev(x1))
+  x <- 0.25 + 0.50*x
+  y <- c(y,rev(y1))
+  y <- 0.50*y
+
+
+  id <- rep(1, length(x))
+  fill <- colfill
+
+  if(plot){
+    grid.newpage()
+    pushViewport(viewport(x=0.5,y=0.5,width=1, height=1,
+                          clip=TRUE))
+    if(fill_symbol){
+      grid.polygon(x, y,
+                   default.unit="native",
+                   id=id,
+                   gp=gpar(fill=fill,
+                           lwd=lwd))
+    }else{
+      grid.polygon(x, y,
+                   default.unit="native",
+                   id=id,
+                   gp=gpar(col=colfill,
+                           lwd=lwd))
+    }
+  }
+
+
+  ll <- list("x"= x,
+             "y"= y,
+             "id" = id,
+             "fill" = fill)
+  return(ll)
+}
+
+## out <- cletter(plot=TRUE)
+## out <- Cletter(plot=TRUE)
