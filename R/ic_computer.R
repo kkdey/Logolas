ad99bf4d1da414b8d8d3b8dad1c9fb36a8aa95ce
Kushal K Dey <kshldey@gmail.com>
2017-07-20 01:23:48 -0700

    added background probability option to neg logo comparisons
    Former-commit-id: b5152fd68f143ee0605f0c9a7cee63bbb8ff8168

--- R/ic_computer.R
+++ R/ic_computer.R
@@ -17,6 +17,13 @@
 #' of the logo plots. If TRUE, one uses the relative proportion of the values in the different
 #' columns of the matrix to determine the height of the bars.
 #'
+#' @param bg The background probability, which defaults to NULL, in which case
+#' equal probability is assigned to each symbol. The user can however specify a
+#' vector (equal to in length to the number of symbols) which specifies the
+#' background probability for each symbol and assumes this background probability
+#' to be the same across the columns (sites), or a matrix, whose each cell specifies
+#' the background probability of the symbols for each position.
+#'
 #' @return A vector of same length as the number of columns in the data, with
 #'         each entry representing information contained in that column,
 #'         which determines the height of the bar for the logo plot for that
@@ -35,16 +42,44 @@
 #' rownames(counts_mat) <- c("P1", "P2", "P3", "P4")
 #' ic_computer(counts_mat, alpha=2)
 
-ic_computer <-function(mat, alpha, hist=FALSE) {
+ic_computer <-function(mat, alpha, hist=FALSE, bg = NULL) {
+
+  if (is.vector(bg)==TRUE){
+    if(length(bg) != dim(mat)[1]){
+      stop("If background prob (bg) is a vector, the length of bg must equal the number of symbols for the logo plot")
+    }else{
+      bgmat <- bg %*% t(rep(1, dim(mat)[2]))
+    }
+  }else if (is.matrix(bg)==TRUE){
+    if(dim(bg)[1] != dim(mat)[1] | dim(bg)[2] != dim(mat)[2]){
+      stop("If background prob (bg) is a matrix, its dimensions must match that of the table")
+    }else{
+      bgmat <- bg
+    }
+  }else {
+    message ("using a background with equal probability for all symbols")
+    bgmat <- matrix(1/dim(mat)[1], dim(mat)[1], dim(mat)[2])
+  }
+
+  bgmat <- apply(bgmat+0.1,2,normalize)
+
   if(!hist){
     mat <- apply(mat, 2, function(x) return(x/sum(x)))
     npos<-ncol(mat)
     ic <-numeric(length=npos)
     for (i in 1:npos) {
       if(alpha == 1){
-        ic[i] <- log(length(which(mat[,i]!=0.00)), base=2) + sum(sapply(mat[, i], function(x) {
-          if (x > 0) { x*log2(x) } else { 0 }
-        }))
+        if(is.null(bg)){
+          ic[i] <- log(length(which(mat[,i]!=0.00)), base=2) + sum(sapply(mat[, i], function(x) {
+            if (x > 0) { x*log2(x) } else { 0 }
+          }))
+        }else{
+          ic[i] <- sum(sapply(1:length(mat[,i]), function(x) {
+            if (x > 0) { mat[x,i]*log2(mat[x,i]) } else { 0 }
+          })) - sum(sapply(1:length(mat[,i]), function(x) {
+            if (x > 0) { mat[x,i]*log2(bgmat[x,i]) } else { 0 }
+          }))
+        }
       }
       else if(alpha == Inf){
         ic[i] <- log(length(which(mat[,i]!=0.00)), base=2) + log(max(mat[,i]))
@@ -53,7 +88,11 @@ ic_computer <-function(mat, alpha, hist=FALSE) {
         stop("alpha value must be greater than 0")
       }
       else{
-        ic[i] <- log(length(which(mat[,i]!=0.00)), base=2) - (1/(1-alpha))* log (sum(mat[,i]^{alpha}), base=2)
+        if(is.null(bg)){
+          ic[i] <- log(length(which(mat[,i]!=0.00)), base=2) - (1/(1-alpha))* log (sum(mat[,i]^{alpha}), base=2)
+        }else{
+          ic[i] <- (1/(1-alpha))* log (sum(bgmat[,i]^{alpha}), base=2) - (1/(1-alpha))* log (sum(mat[,i]^{alpha}), base=2)
+        }
       }
     }
     return(ic)
