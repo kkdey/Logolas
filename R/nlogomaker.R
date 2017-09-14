8295b62486f7ab85c95dd8d176708a18afdc0485
Kushal K Dey <kshldey@gmail.com>
2017-09-14 16:53:35 -0500

    single panel bug fixed
    Former-commit-id: 92b38610b577292bb7b125e2f53b62746ab3c837

--- R/nlogomaker.R
+++ R/nlogomaker.R
@@ -359,7 +359,7 @@ nlogomaker <- function(table,
   }
   #  bottomMargin = ifelse(xaxis, 2 + xaxis_fontsize/3.5, 3)
 
-  if(!single_panel){
+  if(!control$single_panel){
     if(is.null(control$viewport.margin.bottom)){bottomMargin <- ifelse(xaxis, 1 + xaxis_fontsize/3.5, 3)}else{bottomMargin <- control$viewport.margin.bottom}
     if(is.null(control$viewport.margin.left)){leftMargin <- ifelse(xaxis, 2 + xaxis_fontsize/3.5, 3)}else{leftMargin <- control$viewport.margin.left}
     if(is.null(control$viewport.margin.top)){topMargin <- max(ylim)+0.5}else{topMargin <- control$viewport.margin.top}
