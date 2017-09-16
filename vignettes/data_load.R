99f59d9b58f3966bda267fc0e385c618fffdd53e
Kushal K Dey <kshldey@gmail.com>
2017-09-16 17:53:20 -0500

    changed sig16 from txt to rda
    Former-commit-id: d997ddb83127859a0c27f8bf32d2bbcb9627b2b3

--- vignettes/data_load.R
+++ vignettes/data_load.R
@@ -8,3 +8,5 @@
 
 data <- get(load(system.file("extdata", "ALL.2.Rdata",
                              package = "Logolas")))
+dat <- read.table("inst/extdata/sig_16.txt")
+save(dat, file = "inst/extdata/sig_16.rda")
