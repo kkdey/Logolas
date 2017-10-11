3fb8f0f2842af5ac3d2c8184500c4430f74f2fbc
Kushal K Dey <kshldey@gmail.com>
2017-10-10 21:23:57 -0500

    moved test functions out of vignettes

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Phylogenetic abundance of bird species families in two clusters of regions in
+#' Himalayas
+#' @format A matrix with bird species families along rows and the clusters
+#' along columns
+#' @return A matrix with bird species families along rows and the clusters
+#' along columns
+"himalayan_fauna_2_clusters"
+
+
+#' Phylogenetic abundance of bird species families in three clusters of regions in
+#' Himalayas
+#' @format A matrix with bird species families along rows and the clusters
+#' along columns
+#' @return A matrix with bird species families along rows and the clusters
+#' along columns
+"himalayan_fauna_3_clusters"
