a1f1de32a39d495136876b6cda31bc0c7660a6e4
Kushal K Dey <kshldey@gmail.com>
2018-08-24 17:07:42 -0500

    updated documentation for the zero augment function

--- R/zero_augment.R
+++ R/zero_augment.R
@@ -7,18 +7,24 @@
 #' positions along the columns, possibly containing 0 values.
 #' 
 #' @return Returns a new table that augments the zeros in the input \code{table}.
+#' @return eps A small pertutbation value to be added to 0. Default is NULL.
+#'             If \code{table} is a counts matrix, the default changes to 0.5, 
+#'             if \code{table} is a positional weight matrix, the default becomes
+#'             0.001.
 #' @examples 
 #' data("mutation_sig")
 #' zero_augment(mutation_sig)
 #' @export 
 
-zero_augment <- function(table, eps = 1e-10){
+zero_augment <- function(table, eps = NULL){
   fl_table <- floor(table)
   diff <- table[!is.na(table)] - fl_table[!is.na(fl_table)]
   if(sum(abs(diff)) == 0){
     message("The table contains counts")
-    table1 <- table + 0.5
+    if(is.null(eps)){eps <- 0.5}
+    table1 <- table + eps
   }else{
+    if(is.null(eps)){eps <- 1e-03}
     table1 <- table + eps
   }
   return(table1)
